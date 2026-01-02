af76ddaee6ef9e2dceb5ad0f1acf1b11f43bf24c
zhangcf <vite@88.com>
2026-01-02 11:35:13 +0800

    fix(修复稳定的依赖描述): 🐞

--- Package.swift
+++ Package.swift
@@ -10,7 +10,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GTCommonSDK",
-            targets: ["GTCommonSDK","GTCommonSDKResolv"]),
+            targets: ["GTCommonSDK"]
+        ),
+        .library(
+            name: "GTCommonSDKResolv",
+            targets: ["GTCommonSDKResolv"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
